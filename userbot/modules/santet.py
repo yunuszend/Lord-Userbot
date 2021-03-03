561dafb5af1fab2049769d48c533dcbd56523935
yunuszenduserbot <79845960+yunuszenduserbot@users.noreply.github.com>
2021-03-03 01:23:22 +0000

    santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -6,9 +6,9 @@ from userbot.events import register
 @register(outgoing=True, pattern=r'^\.santet(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`ZendYNS Telah Mengaktifkan Perintah Santet Online ツ`")
+    await typew.edit("`King Sadboy Telah Mengaktifkan Perintah Santet Online ツ`")
     sleep(2)
-    await typew.edit("`Mencari Nama Orang Ini...`")
+    await typew.edit("`Mencari si jamet ini...`")
     sleep(1)
     await typew.edit("`Santet Online Segera Dilakukan`")
     sleep(1)
